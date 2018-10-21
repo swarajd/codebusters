f252965bf9c23bd70649829a1ff10b5ffc04c64d
swarajd <swaraj.dhumne@gmail.com>
2018-10-21 14:40:48 -0400

    fixed formatting and annotated another testcase

--- test/ciphers.test.js
+++ test/ciphers.test.js
@@ -38,11 +38,11 @@ test('monoalphabetic cipher', () => {
         keyword: no repeated characters
         rotation: none required
 
-        keyword: codebust
-        rest of letters are: afghijklmnpqrvwxyz (length 18)
+        keyword = codebust
+        rest of letters are = afghijklmnpqrvwxyz (length 18)
         random = 0.5 so placement is at index 9
 
-        plaintext  = afghijklmcodebustnpqrvwxyz
+        plaintext = afghijklmcodebustnpqrvwxyz
 
         random = 0.5 so rotation is 13
         initial letters = abcdefghijklmnopqrstuvwxyz
@@ -63,8 +63,8 @@ test('monoalphabetic cipher', () => {
         keyword: no repeated characters
         rotation: required 
 
-        keyword: wodebust
-        rest of letters are: acfghijklmnpqrvxyz (length 18)
+        keyword = wodebust
+        rest of letters are = acfghijklmnpqrvxyz (length 18)
         random = 0.5 so placement is at index 9
 
         plaintext = acfghijklwodebustmnpqrvxyz
@@ -98,12 +98,12 @@ test('monoalphabetic cipher', () => {
         keyword: repeating characters
         rotation: not required
 
-        keyword: codebusters
-        deduped: codebustr
-        rest of letters are: acfghijklmnpqvxyz (length 17)
+        keyword = codebusters
+        deduped = codebustr
+        rest of letters are = afghijklmnpqvwxyz (length 17)
         random = 0.5 so placement is at index 8
 
-        plaintext = afghijklcodebustrmnpqvxyz
+        plaintext = afghijklcodebustrmnpqvwxyz
 
         random = 0.5 so rotation is 13
         initial letters = abcdefghijklmnopqrstuvwxyz
@@ -111,7 +111,7 @@ test('monoalphabetic cipher', () => {
         
         ciphertext = nopqrstuvwxyzabcdefghijklm
 
-        P = afghijklcodebustrmnpqvxyz
+        P = afghijklcodebustrmnpqvwxyz
         C = nopqrstuvwxyzabcdefghijklm
 
         asdf -> NBXO
@@ -119,8 +119,39 @@ test('monoalphabetic cipher', () => {
     */
     result = ciphers.monoalphabetic('asdf', 'k1', 'codebusters');
     expect(result).toEqual('NBXO');
-    // const k1ResultNoPlace = ciphers.monoalphabetic('asdf', 'k1', 'codebust')
 
+    /*
+        setting: k1
+        keyword: repeated characters
+        rotation: required 
+
+        keyword = vodebusters
+        deduped = vodebustr
+        rest of letters are = acfghijklmnpqwxyz (length 17)
+        random = 0.5 so placement is at index 8
+
+        plaintext = acfghijkvodebustrlmnpqwxyz
+
+        random = 0.5 so rotation is 13
+        initial letters = abcdefghijklmnopqrstuvwxyz
+        rotating yields
+
+        ciphertext = nopqrstuvwxyzabcdefghijklm
+        
+        P = acfghijkvodebustrlmnpqwxyz
+        C = nopqrstuvwxyzabcdefghijklm
+
+        rotation required (v), so we rotate one to the left
+
+        P = acfghijkvodebustrlmnpqwxyz
+        C = opqrstuvwxyzabcdefghijklmn
+
+        asdf -> OCYQ
+    */
+    result = ciphers.monoalphabetic('asdf', 'k1', 'vodebusters');
+    expect(result).toEqual('OCYQ');
+
+    
     // const k2ResultNoRepeat = ciphers.monoalphabetic('asdf', 'k2', 'codebust');
     // const k2ResultRepeat = ciphers.monoalphabetic('asdf', 'k2', 'codebusters');
 
