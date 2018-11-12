35536cbcaa610e52f7e61e74d328f0fda2f626a8
swarajd <swaraj.dhumne@gmail.com>
2018-11-12 11:57:07 -0500

    removed call from code

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,54 +1,44 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+const webpack = require("webpack");
 
-const OUTPUT_DIR = './build';
+const OUTPUT_DIR = "./build";
 
 const plugins = [
   new HtmlWebpackPlugin({
-    title: 'Hyperapp One',
-    template: './src/index.html',
-    filename: path.join(__dirname, './index.html'),
+    title: "Hyperapp One",
+    template: "./src/index.html",
+    filename: path.join(__dirname, "./index.html")
   }),
   new ScriptExtHtmlWebpackPlugin({
-    defaultAttribute: 'defer',
+    defaultAttribute: "defer"
   }),
   new MiniCssExtractPlugin({
-    filename: '[name].[hash].css',
-    allChunks: true,
-  }),
-  new MinifyPlugin(),
+    filename: "[name].[hash].css",
+    allChunks: true
+  })
 ];
 
 module.exports = () => ({
-  entry: [
-    './src/index.js',
-    './styles/app.css',
-  ],
+  entry: ["./src/index.js", "./styles/app.css"],
   output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, OUTPUT_DIR),
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, OUTPUT_DIR)
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        include: [
-          path.resolve(__dirname, './'),
-        ],
+        loader: "babel-loader",
+        include: [path.resolve(__dirname, "./")]
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader?importLoaders=1'
-        ]
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, "css-loader?importLoaders=1"]
+      }
+    ]
   },
-  plugins,
+  plugins
 });
