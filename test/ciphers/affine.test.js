422f03e8705a2933054d0766af654701a65e2518
swarajd <swaraj.dhumne@gmail.com>
2018-11-12 10:55:14 -0500

    going to start to use double quotes instead of single quotes

--- test/ciphers/affine.test.js
+++ test/ciphers/affine.test.js
@@ -1,6 +1,14 @@
-import { affine } from './../../src/util/ciphers.js'
+import { affine } from "./../../src/util/ciphers.js"
 
-test('affine cipher', () => {
-    let result = affine('defendtheeastwallofthecastle', 5, 7);
-    expect(result.ciphertext).toEqual('WBGBUWYQBBHTYNHKKZGYQBRHTYKB');
+test("affine cipher with proper a & b", () => {
+    let result = affine("defendtheeastwallofthecastle", 5, 7);
+    expect(result.ciphertext).toEqual("WBGBUWYQBBHTYNHKKZGYQBRHTYKB");
+})
+
+test("affine cipher without proper a", () => {
+    try {
+        affine("asdf", 13, 8);
+    } catch (e) {
+        expect(e).toEqual("invalid value for 'a'");
+    }
 })
