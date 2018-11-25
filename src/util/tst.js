a366ab96c99e809a4eacffb789c1c53337948bc0
swarajd <swaraj.dhumne@gmail.com>
2018-11-25 12:01:26 -0500

    added a pseudo test generating script

--- src/util/tst.js
+++ src/util/tst.js
@@ -1,8 +1,131 @@
-const util = require("./util.js");
+import engine from "./engine.js";
 
-const p = util.generateRandomResultFromSet(util.primesTo20);
-const q = util.generateRandomResultFromSet(util.primesTo20);
+const state = {
+  problem: {
+    ciphertext: "",
+    hint: ""
+  },
+  solution: "",
+  cipherTypes: [
+    "atbash",
+    "caesar",
+    "monoalphabetic",
+    "affine",
+    "vigenere",
+    "baconian",
+    "hill"
+  ],
+  monoalphabetic: {
+    spaces: true,
+    hint: true,
+    errors: false,
+    xenocrypt: false
+  },
+  /*
+  types are:
+  - encryption
+  - analysis
+  */
+  affine: {
+    types: ["encryption"]
+  },
+  /*
+  types are:
+  - encryption
+  - decryption
+  - crib
+  */
+  vigenere: {
+    types: ["encryption", "decryption"]
+  },
+  /*
+  types are:
+  - encryption
+  - decryption
+  - produce
+  methods are:
+  - matrix
+  - pairs
+  */
+  hill: {
+    types: ["produce"],
+    methods: ["matrix", "pairs"],
+    matrixSizes: [2]
+  }
+};
 
-console.log(p, q);
+let customProblems = [
+  {
+    cipherTypes: ["atbash"]
+  },
+  {
+    cipherTypes: ["caesar"]
+  },
+  {
+    cipherTypes: ["monoalphabetic"],
+    monoalphabetic: {
+      spaces: true,
+      hint: true,
+      errors: false,
+      xenocrypt: false
+    }
+  },
+  {
+    cipherTypes: ["monoalphabetic"],
+    monoalphabetic: {
+      spaces: true,
+      hint: false,
+      errors: false,
+      xenocrypt: false
+    }
+  },
+  {
+    cipherTypes: ["monoalphabetic"],
+    monoalphabetic: {
+      spaces: false,
+      hint: true,
+      errors: false,
+      xenocrypt: false
+    }
+  },
+  {
+    cipherTypes: ["monoalphabetic"],
+    monoalphabetic: {
+      spaces: false,
+      hint: false,
+      errors: false,
+      xenocrypt: false
+    }
+  },
+  {
+    cipherTypes: ["affine"]
+  },
+  {
+    cipherTypes: ["vigenere"]
+  },
+  {
+    cipherTypes: ["baconian"]
+  },
+  {
+    cipherTypes: ["hill"],
+    hill: {
+      types: ["produce"],
+      methods: ["matrix"]
+    }
+  },
+  {
+    cipherTypes: ["hill"],
+    hill: {
+      types: ["produce"],
+      methods: ["pairs"]
+    }
+  }
+];
 
-// const keypair = util.generateKeyPair(p, q);
+for (let i = 0; i < customProblems.length; i++) {
+  const newState = Object.assign({}, state, customProblems[i]);
+  const generatedProblem = engine(newState);
+
+  console.log(generatedProblem);
+  console.log("\n\n\n\n\n\n");
+}
